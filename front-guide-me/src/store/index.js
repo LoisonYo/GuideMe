85ed8767c5818c5afda452624b44041563d45547
gregoire.rebstein <gregoire.rebstein@he-arc.ch>
2021-03-12 19:24:36 +0100

    add axios and router

--- front-guide-me/src/store/index.js
+++ front-guide-me/src/store/index.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios'
 
 Vue.use(Vuex);
+axios.defaults.baseURL = ""; // Link to backend
 
 export default new Vuex.Store({
 	state: {},
--- front-guide-me/src/views/Home.vue
+++ front-guide-me/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="home">
 		<img alt="Vue logo" src="../assets/logo.png" />
+		<router-link :to="{ name: 'Search' }">Search</router-link>
 		<HelloWorld msg="Welcome to Your Vue.js App" />
 	</div>
 </template>
