eb7996bba57c8c96108477fa0679e19269543271
LoisonYo <15814178+LoisonYo@users.noreply.github.com>
2021-03-15 14:01:12 +0100

    Renommage front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <v-app>
-    <v-main>
-      <v-container>
-        <router-view></router-view>
-      </v-container>
-    </v-main>
+	<v-app>
+		<TheHeader/>
+		<v-main>
+			<v-container fluid class="pa-0">
+				<router-view></router-view>
+			</v-container>
+		</v-main>
   </v-app>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
 
 export default {
-  name: 'App',
-  
-  data: () => ({
-    //
-  }),
+	name: 'App',
+	components: {
+		TheHeader,
+	},
+	data: () => ({
+		//
+	}),
 };
 </script>
+
+<style scoped>
+#app {
+	background-color: transparent;
+}
+</style>
