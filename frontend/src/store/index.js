c9a5268208ae2e5668ee99cb92dcced5f455cbd0
LoisonYo <15814178+LoisonYo@users.noreply.github.com>
2021-03-26 10:02:28 +0100

    store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from "vuex";
 import axios from 'axios'
 
 Vue.use(Vuex);
-axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.baseURL = "http://localhost:8000/api/";
 
 export default new Vuex.Store({
 	state:
@@ -17,18 +17,13 @@ export default new Vuex.Store({
 	},
 
 	actions: {
-		register(context, data)
+		async register(context, data)
 		{
 			return new Promise((resolve, reject) => {
-				axios.post("/users", {
-					name: data.name,
+				axios.post("users/", {
+					username: data.name,
 					email: data.email,
 					password: data.password,
-					password_confirmation: data.password_confirmation,
-				})
-				.then(response => {
-					console.log(response)
-					//resolve(response)
 				})
 				.catch(error => {
 					reject(error)
