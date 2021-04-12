4cf6d10f6979f44e29991de5aa67b5d06023f39e
ugo.crucy <ugo.crucy@he-arc.ch>
2021-04-12 15:13:20 +0200

    update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -22,7 +22,7 @@ namespace :django do
     desc 'Apply migrations'
     task :migrate do
         on roles(:web) do |h|
-            execute "cp #{shared_path}/.env #{release_path}/backend/guideme/env"
+            execute "cp #{shared_path}/env #{release_path}/backend/guideme/env"
             execute "source #{venv_path}/bin/activate"
             execute "python3 #{release_path}/backend/guideme/manage.py migrate"
             execute "cd #{release_path}/backend"
