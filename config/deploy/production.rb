ae2bd33e1ba18c50e14f40e54aded6f786398bc0
Ugo Crucy <55437503+Rezerem@users.noreply.github.com>
2021-04-12 15:59:16 +0200

    test with Zest build-up config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,11 +11,6 @@ server "srvz-webapp.he-arc.ch", user: "poweruser", roles: %w{app db web}, port:1
 
 set :deploy_to, "/var/www/#{fetch(:application)}"
 
-after 'deploy:publishing', 'django:migrate'
-after 'django:migrate', 'uwsgi:restart'
-after 'deploy:updating', 'python:update_venv'
-
-
 namespace :uwsgi do
     desc 'Restart application'
     task :restart do
@@ -30,30 +25,88 @@ namespace :python do
         File.join(shared_path, 'env')
     end
 
-    desc 'update venv'
-    task :update_venv do
+    desc 'Create venv'
+    task :create_venv do
         on roles([:app, :web]) do |h|
-            execute "python3 -m venv #{venv_path}"
-            execute "source #{venv_path}/bin/activate" 
-            execute "#{venv_path}/bin/pip install -r #{release_path}/backend/requirements.txt"
+	    execute "python3 -m venv #{venv_path}"
+            execute "source #{venv_path}/bin/activate"
+	    execute "#{venv_path}/bin/pip install -r #{release_path}/back/requirements"
+        end
+    end
+
+    desc 'Config file environement'
+    task :django_config do
+        on roles([:app, :web]) do |h|
+            def config_path
+                File.join(shared_path, 'config/env.py')
+            end
+
+            def upload_src_path
+                File.join(shared_path, "uploads")
+            end
+
+            def upload_target_path
+                File.join(release_path, "back/#{fetch(:projectname)}/uploads")
+            end
+
+            def target_path
+                File.join(release_path, "back/#{fetch(:projectname)}/#{fetch(:projectname)}/env.py")
+            end
+
+            info "config file #{config_path}"
+            if test("[ -f #{config_path} ]")
+                info "Env config file found"
+                
+                if test("[ -f #{target_path} ]")
+                    info "Remove old sym link"
+                    execute "rm #{target_path}"
+                end
+                info "Creating symlink"
+                execute "ln -s #{config_path} #{target_path}"
+            end
+            
+            info "Setup upload symlink"
+            execute "ln -s #{upload_src_path} #{upload_target_path}"
+
+        end
+    end
+
+
+    desc 'Django Migrations'
+    task :django_migration do
+        on roles([:app, :web]) do |h|
+	    execute "#{venv_path}/bin/python #{release_path}/back/zestproject/manage.py migrate"
         end
     end
 end
 
-namespace :django do
-
-    def venv_path
-        File.join(shared_path, 'env')
+namespace :npm do
+    def front_path
+        File.join(release_path, 'front')
     end
 
-    desc 'Apply migrations'
-    task :migrate do
+    desc 'NPM install dependencies'
+    task :install do
         on roles(:web) do |h|
-            execute "#{venv_path}/bin/python #{release_path}/backend/guideme/manage.py migrate"
-	    end
+            execute "cd '#{front_path}'; npm install"
+        end
     end
-    
+
+    desc 'VueJs build app'
+    task :build do
+        on roles(:web) do |h|
+            execute "cd '#{front_path}'; npm run build"
+        end
+    end
 end
+
+after 'deploy:publishing', 'uwsgi:restart'
+after 'deploy:updating', 'python:create_venv'
+after 'python:create_venv', 'python:django_config'
+after 'python:django_config', 'python:django_migration'
+after 'python:django_migration', 'npm:install'
+after 'npm:install', 'npm:build'
+
 
 
 
