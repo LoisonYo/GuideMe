1a1b8729d6b9514125e52a372630ee81a4c4353f
LoisonYo <15814178+LoisonYo@users.noreply.github.com>
2021-04-16 11:16:18 +0200

    add link to activity in MyActivity

--- frontend/src/components/activities/CardEditActivity.vue
+++ frontend/src/components/activities/CardEditActivity.vue
@@ -5,13 +5,10 @@
 	class="ma-5 d-flex justify-space-between" 
 	:class="{'primary darken-1 text--darken-4': !$vuetify.theme.dark}">
 		<div>
-		<div>
-			<v-card-title
-				class="headline"
-				v-text="activity.name">
-			</v-card-title>
-			<v-card-subtitle>{{activity.note}}</v-card-subtitle>
-		</div>		
+			<div>
+				<router-link :to="{ name: 'ActivityDetails', params: { id: activity.id }}" class="secondary--text">{{ activity.name }}</router-link>
+				<v-card-subtitle>{{activity.note}}</v-card-subtitle>
+			</div>
 			<v-btn @click="updateActivity">Modifier</v-btn>
 			<v-btn @click="deleteActivity">Supprimer</v-btn>
 		</div>
