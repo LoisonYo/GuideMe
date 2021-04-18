15d628710b3a6b53d347b46ca4d3657e0c12db1a
LoisonYo <15814178+LoisonYo@users.noreply.github.com>
2021-04-18 13:27:37 +0200

    Fix backend warnings

--- backend/guideme/guidemeapp/models.py
+++ backend/guideme/guidemeapp/models.py
@@ -13,6 +13,7 @@ import os
 # Relations :
 #       Type n <------> n Activity
 class Type(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     icon = models.CharField(max_length=50, default='mdi-border-none-variant')
 
@@ -30,6 +31,7 @@ class Type(models.Model):
 #       Activity n <------> 1 User
 #       Activity 1 <------> n Rating
 class Activity(models.Model):
+    id = models.AutoField(primary_key=True)
     types = models.ManyToManyField(Type)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -50,6 +52,7 @@ class Activity(models.Model):
 #       Rating n <------> 1 Activity
 #       Rating n <------> 1 User
 class Rating(models.Model):
+    id = models.AutoField(primary_key=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     activity = models.ForeignKey(Activity, related_name='ratings', on_delete=models.CASCADE, default=None)
 
