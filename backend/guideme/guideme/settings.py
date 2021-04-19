868bfcc2e5438a7a4cfe401fd603478565e70f5c
LoisonYo <15814178+LoisonYo@users.noreply.github.com>
2021-04-19 12:01:24 +0200

    allowed_host all

--- backend/guideme/guideme/settings.py
+++ backend/guideme/guideme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vtk20xhu65=(-obs1nk5#@fad9^@7qa7+y-)@g&5cqvb^q@n09'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
